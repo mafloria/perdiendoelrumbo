fef3831e21bc7bc26080d4279431d427883e5f21
Alvaro Rojas Tafur <arojastafur@gmail.com>
2017-07-14 14:42:12 -0500

    favico

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
     <!-- Who owns the content of this site? -->
     <meta name="Copyright" content="Save the Children Mexico" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="shortcut icon" href="/favicon.png" type="image/x-icon">
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
     
     <link rel="stylesheet" href="assets/css/style.css" />    
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">        
