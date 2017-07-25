f3ba70b657b458a51afbcd29aa1fdddf2235a4d3
Alvaro Rojas Tafur <arojastafur@gmail.com>
2017-07-25 09:08:03 -0500

    creditos "final"

--- index.php
+++ index.php
@@ -462,52 +462,60 @@
                 <div class="content-intro sign-p">
                     <div class="sign-p-txt">
                         <h2>Créditos</h2>
-                        <h3>EQUIPO DE TRABAJO</h3>
-                        <strong>Save the Children Panamá, presenta</strong><br/> 
+
+                        <strong>Save the Children. Oficina Regional para América Latina y El Caribe, presenta:</strong><br/> 
                         Buscando el Norte <br/><br/>
                         
-                        <strong>Dirección y guión</strong><br />
+                        <strong>Coordinación:</strong><br />
+                        Ivonne Arica<br />
+                        Mónica Kuljich<br />
+                        Laura Marco<br /><br />
+
+                        <strong>Dirección y guión:</strong><br />
                         Carolina Ángel Idrobo <br/><br/> 
                         
-                        <strong>Concepto de ilustraciones</strong><br />
+                        <strong>Concepto de ilustraciones:</strong><br />
                         Martanoemí Noriega <br/><br/>
 
-                        <strong>Concepto de diseño web</strong><br />
+                        <strong>Concepto de diseño web:</strong><br />
                         Álvaro Rojas Tafur <br/><br/>
                         
-                        <strong>Desarrollo y programación</strong><br />
+                        <strong>Desarrollo y programación:</strong><br />
                         Ruth Margaret Florian Caipa <br/><br/>
-                        
-                        <strong>Coordinación Save the Children</strong><br />
-                        Laura Marco <br/>
-                        Ivonne Arica<br/>
-                        Mónica Kuljich<br/><br/>
-                        
-                        <strong>Mapas</strong><br />
-                        Edgar Enrique Figueroa<br/><br/>
-                        
-                        <strong>Audio y diseño sonoro</strong><br />
+
+                        <strong>Mapas:</strong><br />
+                        Edgar Enrique Figueroa<br /><br />
+
+                        <strong>Audio y diseño sonoro:</strong><br />
                         Luis Lasso<br/><br/>
-                        
-                        <strong>Voces</strong><br />
-                        Laura Valentina Noguera Idrobo<br/>
+
+                        <strong>Voces:</strong><br />
+                        Valentina Noguera Idrobo<br/>
                         Paula Andrea Baruque<br/>
                         Santiago Baruque<br/>
                         Michael Griffith<br/>
                         Rodrigo Alvarado Szejner<br/>
-                        Michelle Szejner<br/>
-                        Manuel Antonio Jované Carrillo<br/><br/>
-                        
-                        <strong>Traducción</strong><br />
+                        Michelle Szejner<br/><br/>
+
+                        <strong>Traducción:</strong><br />
                         Álvaro Girón<br/><br/>
                         
-                        <strong>Investigación </strong><br />
-                        Save the Children Honduras, Guatemala, El Salvador<br/><br/>
+                        <strong>Colaboración:</strong><br />
+                        Save the Children en Honduras<br />
+                        Save the Children en Guatemala<br />
+                        Save the Children en El Salvador<br/><br/>
                         
-                        <strong>Producción</strong><br />
+                        <strong>Producción:</strong><br />
                         Pela ́as y Pela ́os te cuentan...<br/><br/>
+                                               
                         
-                        Agradecemos  profundamente  a  los  niños,  niñas  y  adolescentes  del  triángulo  norte  de Centroamérica que hicieron parte del proceso de investigación. Ellos nos abrieron sus espacios y compartieron sus testimonios para visibilizar lo que pasa cuando estás migrando en condiciones de vulnerabilidad, y que sus historias sirvan como referencia para todos aquellos niños, niñas y adolescentes que están pensando tomar la ruta hacia los Estados.
+                        Agradecemos profundamente a los niños, niñas y adolescentes del Triángulo Norte
+                        de Centroamerica que hicieron parte del proceso de investigación. Ellos nos abrieron
+                        sus espacios y compartieron sus testimonios para visibilizar lo que pasa cuando estás
+                        migrando en condiciones de vulnerabilidad, y que sus historias sirvan como
+                        referencia para todos aquellos niños, niñas y adolescentes que están pensando tomar
+                        la ruta hacia los Estados.<br/>
+                        
                         <div class="sign-bottom">
                           <img src="images/logo-save-children.png" width="578" height="146">    
                         </div>                         
