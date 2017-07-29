1d720d9929009b30b3dd2ae5afdacbe6ed3aa07f
Alvaro Rojas Tafur <arojastafur@gmail.com>
2017-07-29 16:19:03 -0500

    historias de vida / coleccion 1

--- index.php
+++ index.php
@@ -84,9 +84,10 @@
             <div class="submenu" style="display: none;">
                 <ul>
                     <li><a href="#sobrenosotros">Sobre Buscando el norte</a></li> 
+                    <li><a href="#historias">Historias de vida</a></li>
                     <li><a href="buscando-el-norte.pdf" download>Buscando el norte [PDF]</a></li>
                     <!--<li><a href="">Perfil del Migrante</a></li>
-                    <li><a href="">Historias de vida</a></li>
+                    
                     <li><a href="">Información para tomadores de decisiones</a></li>
                     <li><a href="">Qué hacer si eres joven migrante</a></li>-->
                     <li><a href="#creditos">Créditos</a></li>
@@ -442,10 +443,108 @@
             <section id="historias" class="section-site buscandoelnorte">
                 <a class="nav-before chapter-nav" href="#introduccion"><svg><use xlink:href="#nav"/></svg></a>           
                 <div class="content-intro sign-p">
-                    <img class="sign-logo-bn" src="images/buscando-el-norte-logo-horizontal-top.png" width="541" height="61">
                     <div class="sign-p-txt">
-                        Más  de  200.000 niños,  niñas  y  adolescentes fueron  detenidos en  la  frontera  sur  de  los Estados  Unidos en los  últimos  3  años. 75%  de ellos provenían  del  Triángulo  Norte  de Centroamérica. 43%  viajaba  sin  compañía  de  un  adulto  o  cuidador ,  aumentando  su vulnerabilidad en  una  ruta  donde  la trata  de  personas, el  tráfico,  la  explotación,  la violencia física y sexual son frecuentes.
-                        <br/><br/> 
+                        <div class="title-info center">
+                            <h2>Historias de Vida</h2> 
+                            <h3><strong>Colección 01: La partida</strong></h3>
+                        </div>
+                        
+                        <div class="h-cases">
+                            <p>Estefany viajó cuando tenía 13 años con un grupo de amigos con quienes experimentó calamidades y peligros. ¡Los más grandes de su vida!.</p>
+                            <div class="photo-cases">
+                                <img src="images/hv/coleccion1-buscando-el-norte-07.jpg">  
+                                <img src="images/hv/coleccion1-buscando-el-norte-05.jpg">       
+                            </div>  
+                            <div class="quote">
+                                <i>Yo intenté emigrar por decisión propia, por ayudar a mi mamá. Lo hice por ella y por mi hermano, para que tuvieran un mejor futuro. Iba sola, con algunos amigos de la misma comunidad, pero no habían adultos en ese grupo.<br />
+                                Para conseguir el dinero del viaje, yo le hacía trabajitos a un muchacho de una pulpería  en las tardes, cuando no iba a la escuela, y así ahorré.<br /> 
+                                Cruzar de Guatemala a México nos costó bastante porque no íbamos con coyote, dos del grupo nos iban guiando. A veces no dormíamos, viajábamos más de noche porque está solitario y en el día hacíamos vigilancia. Es que en el camino hay muchos peligros. Íbamos más hembras que varones y una de ellas sufrió violación, fueron los que se dicen llamar «Z». Los identificamos porque andaban todos tatuados con la letra «Z». Nosotros por temor nos entregamos a la migración.
+                                </i> 
+                                <h4>Estefany - 15 a&ntilde;os</h4>
+                                <p>San Pedro Sula, Honduras</p>  
+                            </div>  
+                        </div>
+                       
+                       <div class="h-cases">
+                            <p>A José Nahúm su familia le prometió que en los Estados Unidos tendría un mejor futuro. </p>
+                            <div class="photo-cases">
+                                <img src="images/hv/coleccion1-buscando-el-norte-09.jpg">  
+                                <img src="images/hv/coleccion1-buscando-el-norte-10.jpg">
+                            </div>
+                            <div class="quote">
+                                <i>Yo estaba en medio de mis estudios cuando decidí irme. Me faltaba un mes para terminar el año, pero la economía estaba mal. Somos seis hermanos y a veces no ajustaba para ninguno. Si había para uno no alcanzaba para el otro. Así que tuve que partir para encontrar una vida, trabajar y estudiar allá, hacer algo por la familia.<br /> 
+                                Me motivaron mis padres -ellos trabajan con préstamos-, y mi tío, que está allá y me dijo que me iba a apoyar y me iba a buscar trabajo cuando cruzara.<br /> 
+                                Al emigrar, el nivel de vida de mi familia puede mejorar…  <br /> 
+                                No llegué tan lejos que se diga, me agarraron aquí adelantito de Chiapas, México, en la primer caseta. Me tuvieron tres horas, iban cayendo más personas y ajustaron el viaje, me llevaron a la cárcel por decirlo así, donde estaban los hondureños, los guatemaltecos, los salvadoreños. Me sentía nervioso, no quería hablar con nadie.
+                                </i>
+                                <h4>José Nahúm-16 a&ntilde;os</h4>
+                                <p>San Pedro Sula, Honduras</p>
+                            </div>
+                       </div>
+
+                       <div class="h-cases">
+                            <p>Gabriela llegó a cuarto grado de primaria y solamente habla idioma Quiché. Un día su padre le dio una noticia que cambiaría su vida.</p>
+                            <div class="photo-cases">
+                                <img src="images/hv/coleccion1-buscando-el-norte-01.jpg">  
+                                <img src="images/hv/coleccion1-buscando-el-norte-11.jpg">
+                            </div>
+                            <div class="quote">
+                                <i>«No es justo que sólo yo conozca los Estados y haga dinero», dijo mi papá un día que vino. Pero no me habló sobre el camino del desierto, ni tampoco cómo o dónde encontraría trabajo. Yo solamente sabía que iba a Texas. Mi mamá me aconsejó que no me fuera y que podía ser peligroso. Igual, me fui. Me puse pantalón porque es más seguro para las mujeres y arrancamos. Nos hicimos pasar por mexicanos porque el «coyote» nos dijo que era lo mejor. Pero no nos creyeron y en la frontera de Guatemala - México nos bajaron de la camioneta. Esperamos en la ruta a las autoridades de migración que nos llevarían a alguna instalación mexicana donde dormiríamos 2 días, nos dieron de comer y el kit de higiene. </i>
+                                <h4>Gabriela - 15 años</h4>
+                                <p>Quiché, Guatemala</p>
+                            </div>
+                       </div>
+
+                       <div class="h-cases">
+                            <p>Luis viajó en automóviles tipo panel, parecidos a los que entregan producto en tiendas pequeñas. </p>
+                            <div class="photo-cases">
+                                <img src="images/hv/coleccion1-buscando-el-norte-12.jpg">  
+                                <img src="images/hv/coleccion1-buscando-el-norte-13.jpg">
+                            </div>
+                            <div class="quote">
+                                <i>Yo había pensado en migrar pero después me arrepentí, ya no me quería ir. Pero como mi mamá ya había sacado el préstamo de Q75,000 ($9,740) del banco y ya estaba pagado el viaje para el lunes, me fui con mi padrastro. Yo pensé irme para trabajar y recuperar el dinero que debíamos. Ni sabía en qué podría trabajar, eso no lo tenía pensado. Solo llegar, trabajar y pagar.<br />
+                                El lunes que salimos me sentía triste. Todo el camino me fui llorando hasta llegar a Xela. De allí poco a poco me fui acoplando al viaje. Fui conociendo más patojos de mi edad y fuimos pasando el camino juntos. Estaba triste por mis amigos, por mi familia pero ya iba montado allí.
+                                Todos apretados, 110 personas, casi no se podía respirar porque no había ventanas. Íbamos camino a McAllen cuando nos detuvo la policía mexicana.<br /> 
+                                Los «coyotes» intentaron sobornarlos, pero no. Nos llevaron a una estación que parecía una cárcel (…) Y allí  nos separaron: hombres, mujeres y niños. 
+                                </i>
+                                <h4>Luis-18 a&ntilde;os </h4>
+                                <p>Quiché, Guatemala</p>
+                            </div>
+                       </div>
+
+                       <div class="h-cases">
+                            <p>Roberto y su familia se fueron huyendo de las extorsiones. El adolescente tuvo que abandonar sus estudios para preservar la vida.</p>
+                            <div class="photo-cases">
+                                <img src="images/hv/coleccion1-buscando-el-norte-14.jpg">  
+                                <img src="images/hv/coleccion1-buscando-el-norte-15.jpg">
+                            </div>
+                            <div class="quote">
+                                <i>Mi madre y mi hermano habían decido partir hacia Los ángeles, California, Estados Unidos. Yo apenas estaba iniciando primer año de bachillerato, no tenia deseos de migrar pero no tuve otra opción más que partir con ellos. Mi madre y mi hermano estaban muy mal
+                                ante la situación crítica del país. Ellos anhelaban encontrar una mejor calidad de vida, se sentían desesperados por las extorsiones de los grupos antisociales. Además la mayor parte de mi familia está en Estados Unidos.<br /> 
+                                El viaje no fue tan duro hasta que llegamos a México. Allí empezaron los problemas con los «coyotes» y nos agarró la policía. Me revisaron de pies a cabeza. Nos llevaron en la «perrera», así le dicen porque es como que ahí llevan a  los perros... Un lugar repleto de hombres de todas la nacionalidades, unos eran «bayuncos» y vulgares, la comida era¡horrible!, yo no comí. Nos separaron: a mi mamá a la zona de mujeres, mi hermano a la zona de hombres y yo a la zona de niños porque como yo no andaba mi pasaporte, porque como ustedes saben, las mentiras que dan, «no anden documentación porque si los agarran va a saber de donde son», y ese es el peor error que uno puede cometer, botar sus documentos.
+                                </i>
+                                <h4>Roberto - 18 a&ntilde;os</h4>
+                                <p>San Salvador, El Salvador</p>
+                            </div>
+                       </div>
+
+                       <div class="h-cases">
+                            <p>Viajar o morir. Esa fue la disyuntiva familiar de Amalia, a quien la vida se le complicó por una pintura.</p>
+                            <div class="photo-cases">
+                                <img src="images/hv/coleccion1-buscando-el-norte-16.jpg">  
+                                <img src="images/hv/coleccion1-buscando-el-norte-17.jpg">
+                                <img src="images/hv/coleccion1-buscando-el-norte-18.jpg">  
+                                <img src="images/hv/coleccion1-buscando-el-norte-19.jpg">
+                            </div>
+                            <div class="quote">
+                                <i>Todo empezó cuando limpiamos el muro frente al parque central para hacer  un mural con unos amigos. Sin querer tapamos un graffiti de  la pandilla de la zona. Ellos se enfurecieron y mataron a un mi amigo que lideraba el proyecto.  Desde ese momento hemos tenido miedo de morir. Como una persecución constante… Mis padres buscaron alternativas para mantenernos seguros y como en los «Estados» tenemos familia decidimos irnos. <br /> 
+                                Cuando nos arrestaron nos llevaron a migración, estuvimos unos días en un cuarto reducido y luego nos llevaron al centro de atención. Nos explicaron que podríamos aplicar para un asilo político en México pero si no calificábamos nos deportarían.
+                                </i>
+                                <h4>Amalia -16 a&ntilde;os</h4>
+                                <p>Mercedes Uma&ntilde;a, El Salvador</p>
+                            </div>
+                       </div>
+
 
                         <div class="intro-chapter-menu inner-page">
                             <h3>Navega por los diferentes capítulos:</h3>
