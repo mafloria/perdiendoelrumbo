da17808d239db2372450e81d848d5b8484768c29
mafloria <margareth.florian@gmail.com>
2017-08-11 19:53:07 -0500

    dos secciones nuevas.
    ALVARO: las meti en archivos por aparte para quitar ese chori..  buscas
    el archivo con el nombre de la seccion y metes las cosas alli

---
+++ sections/tomadoresdecisiones.php
@@ -0,0 +1,21 @@
+          <section id="tomadoresdecisiones" class="section-site">
+                <a class="nav-before chapter-nav" href="#introduccion"><svg><use xlink:href="#nav"/></svg></a>           
+                <div class="content-intro sign-p">
+                    <div class="sign-p-txt">
+                        <h2>Tomadores de Decisiones</h2>
+                        
+                        AQUI HISOTRIETAS
+                        
+                        <div class="sign-bottom">
+                          <img src="images/logo-save-children.png" width="578" height="146">    
+                        </div> 
+
+                        <?php include("sections/navegacion-capitulos.php"); ?>
+
+                    </div>
+                </div>
+                
+                <div class="background-section">
+                    <img class="med-bg" id="imgbg-creditos" src="images/buscando-el-norte-1024-intro.png">
+                </div>
+            </section> <!-- end tomadoresdecisiones -->
